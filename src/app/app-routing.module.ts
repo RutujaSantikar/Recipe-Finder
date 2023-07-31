9d266e897a9ddababfc550ae2ac33054e858a2c1
Rutuja Santikar <santikarrutuja@gmail.com>
2023-07-31 21:45:06 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
 import { MyfavsComponent } from './myfavs/myfavs.component';
 import { SignupComponent } from './auth/signup.component';
 import { RecipelistComponent } from './recipelist/recipelist.component';
